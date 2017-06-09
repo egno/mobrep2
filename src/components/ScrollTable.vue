6c8e325e1af6cc56e9f0a50f0667ebc508a8bb75
Alexandr Shelemetyev <shelemetyev@katren.ru>
2017-06-09 14:06:11 +0700

    fix percent representation

--- src/components/ScrollTable.vue
+++ src/components/ScrollTable.vue
@@ -179,7 +179,7 @@ export default {
       this.$emit('reorder', event)
     },
     percentSwitch (event) {
-      const newVal = (this.totals && this.totals[0]) && (this.headers[event].search('%') === -1) && !(this.showInPercent[event] && this.showInPercent[event].percent)
+      const newVal = (this.totals && this.totals[event] && !isNaN(+(Array.isArray(this.totals[event]) ? this.totals[event][0] : this.totals[event]))) && (this.headers[event].search('%') === -1) && !(this.showInPercent[event] && this.showInPercent[event].percent)
       this.showInPercent[event].percent = newVal
     },
     setHeight () {
