7557c791f1a1f0cf2666ec0515b1de718e92aae6
Alexandr Shelemetyev <shelemetyev@katren.ru>
2017-06-19 11:52:20 +0700

    login message with HTML tags

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,7 +2,7 @@
   <div class="col">
     <div class="row">
       <p><small>
-        Система работает в тестовом режиме.<br>Если обнаружите ошибки, прошу отнестить с пониманием и сообщить о них <a href="mailto:shelemetyev2katren.ru">Александру Шелеметьеву</a>
+        Система работает в тестовом режиме.<br>Если обнаружите ошибки, прошу отнестить с пониманием и сообщить о них <a href="mailto:shelemetyev@katren.ru">Александру Шелеметьеву</a>
       </small>
       </p>
 
@@ -14,30 +14,24 @@
     </div>
     <div class="row">
       <div v-if="!checkLogIn">
-        <form>
           <div class="form-group">
             <label for="email">Учётная запись</label>
-            <input v-model.lazy="form.email" name="username" type="email"
+            <input v-model.trim="form.email" name="username" type="email"
             class="form-control" id="email" placeholder="email"
-             @keyup.enter="gotoPassword"/>
+             @keyup.enter="gotoPassword"
+             />
             <small id="emailHelp" class="form-text text-muted">Введите email, связанный с рабочей учётной записью</small>
           </div>
           <div class="form-group">
             <label for="password">Пароль</label>
-            <input v-model.lazy="form.pass" name="password" type="password"
+            <input v-model="form.pass" name="password" type="password"
             class="form-control" id="password" ref="password" placeholder="Пароль"
-            @keyup.enter="tryLogIn"/>
+            @keyup.enter="tryLogIn"
+            />
           </div>
-          <div>
-            <p>
-              {{ message }}
-            </p>
-            <p v-if="help">
-              Обратитесь в техподдержку <a href="mailto:help@katren.ru">help@katren.ru</a> с заявкой на включение вас в группу пользователей СмартГланс (Nsk-Resource-SmartGlanceUser)
-            </p>
+          <div class="form-group" v-html="message">
           </div>
           <button type="submit" class="btn btn-primary" :disabled='waitStatus' @click="tryLogIn" @keyup.enter="tryLogIn">Войти</button>
-        </form>
       </div>
       <div v-if="checkLogIn">
         <button type="submit" class="btn" @click="goHome"><span class="fa fa-home fa-lg" aria-hidden="true"></span></button>
@@ -65,10 +59,7 @@ export default {
   computed: {
     ...mapGetters([
       'checkLogIn'
-    ]),
-    help () {
-      return this.message === 'Доступ запрещён'
-    }
+    ])
   },
   watch: {
     'token': 'checkToken'
@@ -86,9 +77,14 @@ export default {
       this.$refs.password.select()
     },
     tryLogIn () {
-      this.waitStatus = true
-      this.message = 'Подождите, проверяю права доступа...'
-      this.getTocken()
+      const message = ((this.form.email) ? '' : 'Не указан email. ') + ((this.form.pass) ? '' : 'Не указан пароль. ')
+      if (message) {
+        this.message = message
+      } else {
+        this.waitStatus = true
+        this.message = 'Подождите, проверяю права доступа...'
+        this.getTocken()
+      }
     },
     checkToken () {
       if (this.token) {
@@ -110,18 +106,16 @@ export default {
             }
           },
           (response) => {
-            this.message = 'Ошибка авторизации'
+            this.message = response.body.message || 'Ошибка авторизации'
             this.logOut()
             this.waitStatus = false
           }
         )
         .then((data) => {
-          this.message = 'Ответ получен'
           if ((data) && (data.token)) {
             this.message = 'Доступ разрешён'
             this.token = data.token
           } else {
-            this.message = 'Доступ запрещён'
             this.logOut()
             this.waitStatus = false
           }
